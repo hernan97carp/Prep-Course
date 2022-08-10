bf60ac3e9dcb82ddbe79cac0d76b04be1ef5410f
hernan97carp <hernan97carp@hotmail.com>
2022-08-10 16:15:59 -0300

    firstcommit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -9,6 +9,8 @@ function obtenerMayor(x, y) {
 }
 
 function mayoriaDeEdad(edad) {
+  if (edad >=18){return'Allowed';} 
+  else {return 'Not allowed';}
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
