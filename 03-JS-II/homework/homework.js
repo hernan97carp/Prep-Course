e5b3e0a0527448c525b531c13c2b170781159525
hernan97carp <hernan97carp@hotmail.com>
2022-08-16 20:09:33 -0300

    firstcommit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,7 +1,10 @@
 // No cambies los nombres de las funciones.
 
 function obtenerMayor(x, y) {
-  if (x > y){return x;} {return y;}
+  if (x > y){
+    return x;
+  } 
+return y;
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
